9af8e067c8528938ff7e5e06966a0f7d627f9131
michael2697 <michael2697@hotmail.com>
2024-11-21 21:40:49 +0000

    Add more lab folders and files

--- solutions/01/main.tf
+++ solutions/01/main.tf
@@ -1,1 +1,28 @@
-{\rtf1}
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "3.0.1"
+    }
+  }
+}
+
+provider "docker" {
+  # Configuration options
+}
+
+# Pulls the image
+resource "docker_image" "apache_web" {
+  name  = "httpd:latest"
+}
+
+# Create a container
+resource "docker_container" "web_server" {
+  image = docker_image.apache_web.image_id
+  name  = "web_server"
+  ports {
+    internal = 80
+    #external = 8081
+    external = 88
+  }
+}
