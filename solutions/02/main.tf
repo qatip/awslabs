f94c64d8d5634746602a21e042e12772477cf395
michael2697 <michael2697@hotmail.com>
2024-12-03 11:51:47 +0000

    Add more lab folders and files

--- solutions/02/main.tf
+++ solutions/02/main.tf
@@ -16,11 +16,10 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    #values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-
-    # original entry
+    # new value
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-
+    # original value
+    #values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
   filter {
