02a10c8a9792dd37283077491984cb3ba3b11c42
qatip <michael2697@hotmail.com>
2025-10-04 11:32:20 +0100

    Platform fixes

--- 04/main.tf
+++ 04/main.tf
@@ -13,6 +13,7 @@ provider "aws" {
     key_prefixes = ["ca-"]
   }
 }
+
 resource "aws_instance" "example" {
   ami           = "ami-0d08c3b92d0f4250a"
   instance_type = "t2.micro"
@@ -22,6 +23,7 @@ resource "aws_instance" "example" {
     Name = "terraform-demo-${terraform.workspace}-${format("%02d", count.index + 1)}"
   }
 }
+
 
 output "instance_details" {
   description = "Details of the created instances"
