4a7ed4d3d36efd4d32392236fea18d19dbd1979d
qatip <michael2697@hotmail.com>
2026-01-29 17:27:02 +0000

    Update create_ec2.sh

--- 06/create_ec2.sh
+++ 06/create_ec2.sh
@@ -73,7 +73,7 @@ sudo apt update -y && sudo apt upgrade -y
 sudo apt install -y openjdk-17-jdk unzip curl gnupg lsb-release
 
 # Add Jenkins repository and install specific version
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update -y
 sudo apt install -y jenkins=$JENKINS_VERSION
